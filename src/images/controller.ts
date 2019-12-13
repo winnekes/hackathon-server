41ab698e00120834d398d49279bf08bf628d519d
Simona Winnekes <simona.winnekes@googlemail.com>
2019-12-13 08:09:47 +0100

    Only return image url now after file write

--- src/images/controller.ts
+++ src/images/controller.ts
@@ -33,6 +33,7 @@ export default class ImageController {
     @Authorized()
     @Post('/images/:eventId')
     async handleFileUpload(
+        @Ctx() ctx: any,
         @UploadedFile('file') file: any,
         @CurrentUser() user: User,
         @Param('eventId') eventId: number,
@@ -41,7 +42,7 @@ export default class ImageController {
         const fileName = uuidv1();
         const path = `/media/simona/DATA/travelin-images/${fileName}`;
         const url = `/images/${fileName}`;
-        await fs.writeFile(path, file.buffer, async err => {
+        await fs.writeFile(path, file.buffer, err => {
             if (err) {
                 console.log('image upload failed:', err);
                 response.status = 500;
@@ -50,32 +51,33 @@ export default class ImageController {
                 };
                 return;
             }
-            const image = Image.create();
-            image.id = fileName;
-            image.mimeType = file.mimetype;
-            image.createdAt = new Date();
-            const event = await Event.findOne(eventId);
-            if (event) image.event = event;
-            image.url = url;
-            image.user = user;
-            image.private = false;
-
-            const exifResult = await exifr.parse(path);
-            console.log(exifResult);
-            if (exifResult.DateTimeOriginal) {
-                image.createdAt = exifResult.DateTimeOriginal;
-            } else if (exifResult.timestamp) {
-                image.createdAt = exifResult.timestamp;
-            }
-            if (exifResult.latitude && exifResult.longitude) {
-                image.latitude = exifResult.latitude;
-                image.longitude = exifResult.longitude;
-            }
-            console.log(await image.save());
         });
+        const image = Image.create();
+        image.id = fileName;
+        image.mimeType = file.mimetype;
+        image.createdAt = new Date();
+        const event = await Event.findOne(eventId);
+        if (event) image.event = event;
+        image.url = url;
+        image.user = user;
+        image.private = false;
 
+        const exifResult = await exifr.parse(file.buffer);
+        console.log('buf', file.buffer);
+        console.log('exif', exifResult);
+        if (exifResult.DateTimeOriginal) {
+            image.createdAt = exifResult.DateTimeOriginal;
+        } else if (exifResult.timestamp) {
+            image.createdAt = exifResult.timestamp;
+        }
+        if (exifResult.latitude && exifResult.longitude) {
+            image.latitude = exifResult.latitude;
+            image.longitude = exifResult.longitude;
+        }
+        console.log(await image.save());
+        ctx.type = 'application/json';
         response.status = 201;
-        response.body = { url };
+        response.body = { url: image.url };
         return response;
     }
 
